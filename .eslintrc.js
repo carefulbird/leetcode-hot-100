84bedc492522140579e1b49a24791076ebdab047
hao.lilh <hao.lilh@shopee.com>
2022-10-08 11:12:25 +0800

    feat: map method

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,17 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
+    es2021: true,
   },
   extends: ['eslint:recommended'],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 'latest',
   },
   rules: {
     // http://eslint.cn/docs/rules/
     'no-console': 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': 'warn',
     // 使用2个空格
     indent: ['error', 2, { SwitchCase: 1 }],
     semi: 'error',
--- LRU  缓存/map-methos.js
+++ LRU  缓存/map-methos.js
@@ -1,4 +1,12 @@
 /**
+ *
+ * 思路：
+ * 1、利用 map 能够取出最后的 key 特性
+ * 2、map set 自动放到第一个
+ * 3、
+ */
+
+/**
  * @param {number} capacity
  */
 var LRUCache = function (capacity) {
@@ -12,6 +20,11 @@ var LRUCache = function (capacity) {
  */
 LRUCache.prototype.get = function (key) {
   const res = this.map.get(key)
+  if (res !== 0 && !res) {
+    return -1
+  }
+  this.put(key, res)
+  return res
 }
 
 /**
@@ -19,4 +32,13 @@ LRUCache.prototype.get = function (key) {
  * @param {number} value
  * @return {void}
  */
-LRUCache.prototype.put = function (key, value) {}
+LRUCache.prototype.put = function (key, value) {
+  if (this.map.has(key)) {
+    this.map.delete(key)
+  }
+  this.map.set(key, value)
+  if (this.map.size > this.maxSize) {
+    const key = this.map.keys().next().value
+    this.map.delete(key)
+  }
+}
