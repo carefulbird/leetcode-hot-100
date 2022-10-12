39d7f4c81053be47c0e6452f82615172b74a31d1
hao.lilh <hao.lilh@shopee.com>
2022-10-12 18:54:40 +0800

    feat: add pretterrr

--- LRU  缓存/index.js
+++ LRU  缓存/index.js
@@ -1,16 +1,16 @@
 /**
- *请你设计并实现一个满足  LRU (最近最少使用) 缓存 约束的数据结构。
+ *请你设计并实现一个满足 LRU (最近最少使用) 缓存 约束的数据结构。
 实现 LRUCache 类：
-LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存
+LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存
 int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
-void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；如果不存在，则向缓存中插入该组 key-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。
+void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；如果不存在，则向缓存中插入该组 key-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。
 函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。
 
 
 https://www.jstips.co/zh_cn/javascript/map-to-the-rescue-adding-order-to-object-properties/
 
 object 无顺序
-es6 map 有顺序 
+es6 map 有顺序
 
 示例：
 
@@ -42,26 +42,26 @@ lRUCache.get(4);    // 返回 4
  * @param {number} capacity
  */
 var LRUCache = function (capacity) {
-  this.maxSize = capacity
-  this.keyArray = []
-  this.map = {}
-}
+  this.maxSize = capacity;
+  this.keyArray = [];
+  this.map = {};
+};
 
 /**
  * @param {number} key
  * @return {number}
  */
 LRUCache.prototype.get = function (key) {
-  const index = this.keyArray.indexOf(key)
+  const index = this.keyArray.indexOf(key);
   if (index === -1) {
-    return -1
+    return -1;
   }
-  const res = this.map[key]
-  this.keyArray.splice(index, 1)
-  delete this.map[key]
-  this.put(key, res)
-  return res
-}
+  const res = this.map[key];
+  this.keyArray.splice(index, 1);
+  delete this.map[key];
+  this.put(key, res);
+  return res;
+};
 
 /**
  * @param {number} key
@@ -69,17 +69,17 @@ LRUCache.prototype.get = function (key) {
  * @return {void}
  */
 LRUCache.prototype.put = function (key, value) {
-  const index = this.keyArray.indexOf(key)
+  const index = this.keyArray.indexOf(key);
   if (index !== -1) {
-    this.keyArray.splice(index, 1)
+    this.keyArray.splice(index, 1);
   }
-  this.keyArray.unshift(key)
-  this.map[key] = value
+  this.keyArray.unshift(key);
+  this.map[key] = value;
   if (this.keyArray.length > this.maxSize) {
-    const lastKey = this.keyArray.pop()
-    delete this.map[lastKey]
+    const lastKey = this.keyArray.pop();
+    delete this.map[lastKey];
   }
-}
+};
 
 /**
  * Your LRUCache object will be instantiated and called as such:
--- README.md
+++ README.md
@@ -6,4 +6,8 @@ leetcode 热题 100
 
 [leetcode 热题 100](https://leetcode.cn/problem-list/2cktkvj/)
 
-##
+## 开始
+
+```
+pnpm install
+```
