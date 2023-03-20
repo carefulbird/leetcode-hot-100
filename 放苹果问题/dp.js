4fc9455ea8ceed9b640e75a201cb036c2a512b99
520vicky <1596080551@qq.com>
2023-03-21 00:02:26 +0800

    feat:

--- 放苹果问题/dp.js
+++ 放苹果问题/dp.js
@@ -1,17 +1,23 @@
 function findApple(m, n) {
-    let ans = 0;
-    if(n<=1||m<=1){
-        return 1
-    }
-    // 如果盘子比苹果多，那么
-    if (n > m) {
-        return findApple(m, m);
-    }else {
-        // 如果苹果多，那么盘子就会是 全部占满  ==> 只占一个，依次累加
-        // 如果盘子全部铺满，那么就换算成 m- i，i 问题再递归
-        for(let i=1;i<=n;i++){
-            ans = findApple(m-i,i) + ans
+    const dp = new Array(m+1).fill(new Array(n+1).fill(1));
+    for (let i = 2; i <=m; i++) {
+        for (let j = 2; j <= n; j++) {
+            // j 盘子
+            // i 苹果
+            if(j>i){
+                dp[i][j] = dp[i][i]
+            }else {
+                for (let k = 1; k <=j; k++) {
+                    // k 表示现在苹果占了几个盘子
+                    dp[i][j] = dp[i][j] + dp[i-k][k]
+                }
+            }
+
+            
         }
+        
     }
-    return ans
+    return dp[m][n]
 }
+
+// 
